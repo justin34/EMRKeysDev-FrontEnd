a26c8d13c5e3951e86aedaee046b02af2302eeeb
Jabril Hall <jabril6187@gmail.com>
2023-06-27 13:32:36 -0400

    Navbar dropdown not working but im about to change the calendar directory to make things more stremline

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import {DayPilot, DayPilotCalendar, DayPilotNavigator} from "@daypilot/daypilot-lite-react";
+import React, {useRef, useState} from "react";
+import NavBar from "./navBar/NavagationBar.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+
+const styles = {
+    wrap: {
+        display: "flex"
+    },
+    left: {
+        marginRight: "10px"
+    },
+    main: {
+        flexGrow: "1"
+    }
+};
 function App() {
+  const [config, setconfig] = useState({
+    viewType: 'Week'
+  });
+    const calendarRef = useRef();
+
+    const handleTimeRangeSelected = args => {
+        calendarRef.current.control.update({
+            startDate: args.day,
+
+        });
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <NavBar/>
+        <div style={styles.wrap}>
+            <div style={styles.left}>
+                <DayPilotNavigator
+                    selectMode={"Week"}
+                    showMonths={1}
+                    skipMonths={1}
+                    onTimeRangeSelected={handleTimeRangeSelected}
+                />
+            </div>
+
+            <div style={styles.main}>
+                <DayPilotCalendar {...config} ref={calendarRef} />
+            </div>
+        </div>
+
     </div>
   );
 }
