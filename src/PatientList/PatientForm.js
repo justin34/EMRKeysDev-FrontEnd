3a9df51a7045f49e419242bd5b564a209d64c75a
Jabril Hall <jabril6187@gmail.com>
2023-08-21 12:29:11 -0400

    Created Psyc Assesment tab you can add symptoms and declare the severity with a slider. Integrated chatgpt not generation with backend

--- src/PatientList/PatientForm.js
+++ src/PatientList/PatientForm.js
@@ -1,27 +1,103 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import {useParams} from "react-router-dom";
 import axios from "axios";
 import './PatientForm.css'
 import Diagnosis from './Tabs/Diagnosis';
-import { Tabs , TabItemType, Card,Space , Layout} from 'antd';
+import {Tabs, Card, Layout, Button, Input, DatePicker} from 'antd';
+import { UploadOutlined, UserOutlined } from '@ant-design/icons';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import dayjs from "dayjs";
 
 const { Header, Content, Footer, Sider } = Layout;
 
 
 const PatientForm = () =>{
     const [patientData, setPatientData] =  useState({
-        "id": 8,
-        "name": "Marry hill",
-        "DOB": "1988-11-16",
+        "id": 1,
+        "name": "Jane Doe",
+        "DOB": "1999-09-09",
         "notes": "This is the note",
         "profile_picture": "/media/profilePictures/DefaultProfilePic.png"
     });
+    const [editPacient, setEditPacient] = useState(true)
     const params = useParams()
     const patientId = params.patientId
     const userId =  params.userId
+
     const [editorValue, setEditorValue] = useState('');
+    const [doUpdateNote, setDoUpdateNote] = useState(true)
+
+
+    const [profilePicture, setProfilePicture] = useState();
+    const [pacientName, setPacientName] = useState("");
+    const [DOB, setDOB] = useState();
+
+
+
+    function UpdateNote({ children }){
+        if(!doUpdateNote)
+            return children
+        let childWithProps = React.cloneElement(children, {
+                value: editorValue
+            })
+        setDoUpdateNote(false)
+
+        return childWithProps
+    }
+
+    function addTextToNote(text, replace=false)  {
+        if(replace)
+            setEditorValue(text);
+        else
+            setEditorValue(editorValue + text);
+        setDoUpdateNote(true)
+
+    }
+
+    const toggleEditPacient = () => {
+
+        setEditPacient(!editPacient);
+
+        if(!editPacient){
+
+            if(!pacientName)
+                setPacientName(patientData.name);
+            if(profilePicture === null)
+                return;
+            if(!(DOB instanceof dayjs))
+                setDOB(dayjs(patientData.DOB, "YYYY-MM-DD"));
+
+            const newPatientData = new FormData();
+
+            setEditorValue(editorValue + "<strong>Editing</strong>")
+            setDoUpdateNote(true)
+
+            newPatientData.append('name', pacientName);
+            newPatientData.append('DOB', dayjs(DOB).format('YYYY-MM-DD'));
+            //newPatientData.append('profile_picture', profilePicture,  profilePicture.name);
+            console.log(newPatientData)
+
+            axios({
+                method:"post",
+                url: "http://localhost:8000/patients/" + patientId,
+                data: newPatientData,
+                headers: {"Content-Type": "multipart/form-data"}
+            }).then(function (res){
+                console.log(res);
+            }).catch(function (res){
+                console.log(res);
+            })
+            setPacientName(patientData.name);
+            setDOB(dayjs(patientData.DOB, "YYYY-MM-DD"));
+
+
+        }
+    }
+
+    const setDate = (date, dateString) => {
+        setDOB(dateString);
+    }
 
     axios.get("http://localhost:8000/patients/" + patientId).then(res => {
 
@@ -30,6 +106,7 @@ const PatientForm = () =>{
                 setPatientData(patient);
 
     })
+
 
 
 
@@ -50,18 +127,31 @@ const PatientForm = () =>{
                     style={{
                         padding: "10px",
                         margin: "10px",
-                        width: "min-content",
+                        width: "200",
                         height: "min-content",
                     }
                     }
 
                 >
+                    {
+                        editPacient?<div>
                     <img src={"http://localhost:8000" + patientData.profile_picture}
                          alt={"Patient Photo"}
                         width={200}
                         height={200}/>
                     <h2>{patientData.name}</h2>
                     <p>{patientData.DOB}</p>
+                        </div>:<div>
+                            <Input type="file" onChange={(file) => setProfilePicture(file.target.files[0])}/>
+                            <Input size="large" placeholder={patientData.name} prefix={<UserOutlined />} onChange={(name) => setPacientName(name.target.value)} />
+                            <DatePicker defaultPickerValue={dayjs(patientData.DOB, "YYYY-MM-DD")} defaultValue={dayjs(patientData.DOB, "YYYY-MM-DD")} onChange={setDate} />
+
+
+                        </div>
+                }
+                    {
+                    editPacient?<Button type='primary' onClick={() => toggleEditPacient()}>Edit Patient</Button>: <Button type='primary' onClick={() => toggleEditPacient()}>Done</Button>
+                }
                 </Card>
             </Sider>
             <Layout
@@ -72,7 +162,7 @@ const PatientForm = () =>{
                         items={[{
                             label: "Current Symptoms",
                             key: 1,
-                            children: <Diagnosis/>,
+                            children: <Diagnosis updateText={addTextToNote}/>,
 
                         },
                         ]
@@ -84,13 +174,24 @@ const PatientForm = () =>{
                     style={{backgroundColor:"whitesmoke",}
                     }
                     width={"max-content"}>
-
-                    <ReactQuill
-                        value={editorValue}
-                        onChange={(value) => setEditorValue(value)}
-                        style={{width : "500px",
+                    {
+                        doUpdateNote?<UpdateNote>
+                            <ReactQuill
+                                value={editorValue}
+                                onChange={(value) => {setEditorValue(value);
+                                    console.log(editorValue);}}
+                                style={{width : "500px",
+                                    height: "90%"}}
+                            /></UpdateNote>:<ReactQuill
+                            value={editorValue}
+                            onChange={(value) => {setEditorValue(value);
+                                console.log(editorValue);}}
+                            style={{width : "500px",
                                 height: "90%"}}
-                    />
+                        />
+                    }
+
+
 
                 </Sider>
 
--- src/PatientList/Tabs/Diagnosis.js
+++ src/PatientList/Tabs/Diagnosis.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 import {useParams} from "react-router-dom";
 import {Select, Slider, Button} from "antd";
+import axios from "axios";
 
-const Diagnosis = () => {
+const Diagnosis = ({ updateText }) => {
 
     const options = [{label:"Aggression", value: "Aggression"}, {label:"Agitation", value: "Agitation"},
                                               {label:"Anger", value: "Anger"}, {label:"Anxiety", value: "Anxiety"}];
 
+    const [dataMap, setDataMap] = useState([])
+
     const [symptoms, setSymptoms] = useState([])
+
+    const [aINote, setAINote] = useState("")
+
+    const patientId = useParams().patientId
+
+
+
 
     const marks = {
         0: "None",
@@ -17,35 +27,125 @@ const Diagnosis = () => {
         100: "Severe",
     }
 
-    const handleChange = (value, i) => {
+    const handleSympSelect = (value, i) => {
+        let temp = symptoms
+        if(temp.length <= i){
+            temp = [...symptoms, {
+                symptom: value,
+                severity: 0
+            }]
+            setSymptoms(temp)
+            return;
+        }
 
+        temp[i].symptom = value
+        setSymptoms(temp)
     };
 
+    const requestNote = () => {
+        const tempformatedSymptoms = symptoms;
+        const lisToRem = []
+
+        for (let i = 0; i < tempformatedSymptoms.length; i++){
+            switch (symptoms[i].severity){
+                case 0:
+                    lisToRem.push(i);
+                    break;
+                case 25:
+                    tempformatedSymptoms[i].severity = "mild";
+                    break;
+                case 50:
+                    tempformatedSymptoms[i].severity = "moderate";
+                    break;
+                case 75:
+                    tempformatedSymptoms[i].severity = "moderate to severe";
+                    break;
+                case 100:
+                    tempformatedSymptoms[i].severity = "severe";
+                    break;
+                default:
+                    lisToRem.push(i)
+
+            }
+
+            for(let i = 0; lisToRem.length; i++){
+                tempformatedSymptoms.splice(lisToRem[i], 1)
+            }
+
+
+             let formatedSymptoms = { "symptoms": tempformatedSymptoms}
+
+            updateAndPost(formatedSymptoms)
+
+        }
+
+
+    }
+
+    const updateAndPost = (data) => {
+
+        axios({
+            method:"post",
+            url: "http://localhost:8000/ainotes/" + patientId,
+            data: data,
+            headers: {"Content-Type": "application/json"}
+        }).then(function (res){
+            console.log(res);
+            axios.get("http://localhost:8000/ainotes/" + patientId).then(res => {
+                console.log(res.data)
+
+                updateText(res.data[0]["note"])
+
+            })
+        }).catch(function (res){
+            console.log(res);
+        })
+
+
+    }
+
+    const handleSevaSelect = (value, i) => {
+      const temp = symptoms;
+      if(temp.length <= i) {
+          alert("You must select a symptom first")
+          return;
+      }
+      temp[i].severity = value;
+      setSymptoms(temp);
+
+      console.log(JSON.stringify(symptoms))
+    }
+
     const handleAdd = () =>{
-        const symptomList = [...symptoms, []]
-        setSymptoms(symptomList)
+        const dataList = [...dataMap, []]
+        if(dataList.length === 1)
+            updateText("<u><strong><h2>Psych Assessment</h2></strong></u>")
+        setDataMap(dataList)
     }
 
     return(
         <>
-        {symptoms.map((data, i) => {
+        {dataMap.map((data, i) => {
             return(
-                <div>
+                <div key={i}>
                 <Select
                     mode="single"
                     allowClear
                     showSearch={true}
                     style={{ width: '100%' }}
                     placeholder="Please select"
-                    onChange={symp => handleChange(symp, i)}
+                    onChange={symp => handleSympSelect(symp, i)}
                     options={options}
                 />
-                <Slider marks={marks} step={null} defaultValue={0} />
+                <Slider marks={marks} step={null} defaultValue={0} onChange={(value) => handleSevaSelect(value, i)} />
                 </div>
         )
 
             })}
             <Button type="primary" onClick={() => handleAdd()}>Add</Button>
+            {
+                symptoms.length >= 1?<Button type="primary" onClick={() => requestNote()}>Generate Note</Button>:<></>
+            }
         </>
 
 
